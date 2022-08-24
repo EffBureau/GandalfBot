6da78085bec1744962c3d29c1adc7ff7ac505865
Francis <Francis.bureau2@gmail.com>
2022-08-23 23:29:38 -0400

    Add disconnect timer

--- src/bot/utils/utils.py
+++ src/bot/utils/utils.py
@@ -36,3 +36,25 @@ class Utils(commands.Cog):
         voice_client.pause() # Pause the client to let it set up the stream
         await asyncio.sleep(2) # Letting the bot sleep fixes an issue with the player going fast for the first couple of seconds
         voice_client.resume()
+    
+    # Source: https://stackoverflow.com/questions/63658589/how-to-make-a-discord-bot-leave-the-voice-channel-after-being-inactive-for-x-min
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        global client         
+
+        if not member.id == client.user.id:
+            return
+
+        elif before.channel is None: 
+            voice = after.channel.guild.voice_client
+            time = 0
+
+            while True: # This while loop checks if the bot is playing audio
+                await asyncio.sleep(1)
+                time = time + 1
+                if voice.is_playing() and not voice.is_paused(): # If it's playing or paused, reset timer
+                    time = 0
+                if time == 600: # If it gets to 10 minutes, disconnect
+                    await voice.disconnect()
+                if not voice.is_connected():
+                    break
